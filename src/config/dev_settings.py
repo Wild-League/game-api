eaa40dc531ef7c896a3319792c66ce1dee73fbfd
ropoko <rodrigostramantinoli@gmail.com>
2024-03-12 21:19:26 -0300

    chore: added cors headers

--- src/config/dev_settings.py
+++ src/config/dev_settings.py
@@ -54,12 +54,16 @@ INSTALLED_APPS = [
 	'django.contrib.staticfiles',
 	'rest_framework',
 	'rest_framework.authtoken',
-	'src.api.apps.ApiConfig'
+	'src.api.apps.ApiConfig',
+	'corsheaders'
 ]
+
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
 
 MIDDLEWARE = [
 	'django.middleware.security.SecurityMiddleware',
 	'django.contrib.sessions.middleware.SessionMiddleware',
+	'corsheaders.middleware.CorsMiddleware',
 	'django.middleware.common.CommonMiddleware',
 	'django.middleware.csrf.CsrfViewMiddleware',
 	'django.contrib.auth.middleware.AuthenticationMiddleware',
